4fa3345afd3268caf0cc1097520c45f56544bff0
kisana <yuto01200120yuto@gmail.com>
2024-04-18 23:18:39 +0900

    [ADD]フォローされています

--- src2/pages/nodeinfo/2.1.js
+++ src2/pages/nodeinfo/2.1.js
@@ -4,14 +4,14 @@ import FullAppUrl from '@/components/full_app_url'
 export async function getServerSideProps({ req, res, context, query }) {
   const data = {
     "version": "2.1",
-    "software": { "name": "amiverse", "version": "0.1.0" },
+    "software": { "name": "amiverse", "version": "0.0.4" },
     "protocols": ["activitypub"],
     "services": { "inbound": [], "outbound": ["atom1.0", "rss2.0"] },
     "openRegistrations": true,
     "usage": { "users": { "total": 1, "activeHalfyear": null, "activeMonth": null },
     "localPosts": 1, "localComments": 0 },
     "metadata": { "nodeName": "Amiverse.net", "nodeDescription": "Amiverseです。",
-      "maintainer": { "name": "Kisana", "email": "https://kisana.amiverse.net/" },
+      "maintainer": { "name": "Kisana", "email": "https://amiverse.net/@kisana" },
       "langs": ["ja", "en"],
       "tosUrl": "http://amiverse.net/tos",
       "repositoryUrl": "https://github.com/kisana528/amiverse",
